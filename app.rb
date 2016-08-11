67ecc46218df54472c9166cc432359a045858e89
Charlie Cook <turingbombe@gmail.com>
2016-08-11 14:10:29 -0400

    basic routes completed

--- app.rb
+++ app.rb
@@ -1,4 +1,20 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
+
+  get '/' do
+    "Hello, World!"
+  end
+
+  get '/name' do
+    "My name is Charlie"
+  end
+
+  get '/hometown' do
+    "My hometown is New Canaan"
+  end
+
+  get '/favorite-song' do
+    "My favorite song is Back Dat Azz Up"
+  end    
 end
